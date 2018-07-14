45a3a58bd3ef890976e5ffc358a905288e68635a
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-07-14 09:20:38 -0300

    Logging messages with object names

--- Source/BuildingScape/PositionReporter.cpp
+++ Source/BuildingScape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -9,7 +10,6 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 }
 
 
@@ -18,7 +18,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty at %s!"), *ObjectName);
 	
 }
 
